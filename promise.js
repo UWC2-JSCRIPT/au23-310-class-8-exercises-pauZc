3d38309ce908a8ffa2ca7b6a7210c634c97db549
Laura Zanella <zclaurap@gmail.com>
2023-12-02 16:24:55 -0800

    Promise

--- promise.js
+++ promise.js
@@ -1,20 +1,27 @@
-let myPromise = new Promise(function(resolve, reject) {
-  setTimeout(function() {
-    resolve();
-  }, 1000);
-});
+// let myPromise = new Promise(function(resolve) {
+//   setTimeout(function() {
+//     resolve();
+//   }, 1000);
+// });
 
-myPromise
-  .then(function() {
-    return 99;
-  })
-  .then(function(number) {
-    console.log(number);
-  });
+// myPromise
+//   .then(function() {
+//     console.log('success')
+//   })
+//   .then(function(number) {
+//     console.log(number);
+//   })
+//   .catch(function(){
+//     console.log('fail')
+//   })
+//   .finally(function(){
+//     console.log('complete')
+//   });
 
 const randomNumberPromise = new Promise((res, rej) =>{
   setTimeout(() => {
     const randomVal = Math.random()
+    console.log(`randomVal ${randomVal}`)
     if( randomVal > 0.5 ){
       res(randomVal)
     }else if(randomVal <= 0.5){
@@ -26,3 +33,9 @@ const randomNumberPromise = new Promise((res, rej) =>{
 randomNumberPromise.then((value) => {
   console.log('Success')
 })
+.catch(function(){
+    console.log('fail')
+  })
+.finally(function(){
+    console.log('complete')
+});
